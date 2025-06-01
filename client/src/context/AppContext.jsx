fbef88530ad128e33568affdc958ce01276c03d0
Naman_ptdr <namanpatidar5555@gmail.com>
2025-06-01 18:09:20 +0530

    Commponents - Hero Section , JobListings, & JobCards, by AppContext

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,11 +1,32 @@
-import { createContext } from "react";
-
+import { createContext, useState} from "react";
+import { jobsData } from "../assets/assets";
+import { useEffect } from "react";
 
 export const AppContext = createContext()
 
 export const AppContextProvider = (props) => {
-    const value = {
+    const [searchFilter, setSearchFilter] = useState({
+        title:'',
+        location:''
+    })
 
+    const [isSearched, setIsSearched] = useState(false)
+
+    const [jobs, setJobs] = useState([])
+    // function to fetch jobs
+    const fetchJobs = async () => {
+        setJobs(jobsData)
+    }
+
+    useEffect(()=>{
+        fetchJobs()
+    }, [])
+
+
+    const value = {
+        setSearchFilter, searchFilter,
+        isSearched, setIsSearched,
+        jobs, setJobs
     }
     return (<AppContext.Provider value={value}>
         {props.children}
