a32bb75177037a24f4f67bb167784ebaa463e3fb
Naman_ptdr <namanpatidar5555@gmail.com>
2025-06-03 18:03:32 +0530

    Apply Job page

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { AppContext } from '../context/AppContext'
 import { useContext } from 'react'
 import { assets, JobCategories, JobLocations} from '../assets/assets'
@@ -10,6 +10,38 @@ const JobListing = () => {
     const [currentPage, setCurrentPage] = useState(1);
     const [selectedCategories, setSelectedCategories] = useState([])
     const [selectedLocations, setSelectedLocations] = useState([])
+
+    const [filteredJobs, setFilteredJobs] = useState(jobs)
+
+    const handleCategoryChange = (category) =>{
+        setSelectedCategories(
+            prev => prev.includes(category) ? prev.filter(c => c !== category) : [...prev, category]
+        )
+    }
+
+    const handleLocationChange = (location) =>{
+        setSelectedLocations(
+            prev => prev.includes(location) ? prev.filter(c => c !== location) : [...prev, location]
+        )
+    }
+
+    useEffect(() => {
+        const matchesCategory = job => selectedCategories.length === 0 || selectedCategories.includes(job.category)
+
+        const matchesLocations = job => selectedLocations.length === 0 || selectedLocations.includes(job.location)
+
+        const matchesTitle = job => searchFilter.title === ""|| job.title.toLowerCase().includes(searchFilter.title.toLowerCase())
+
+        const matchesSearchLocation = job => searchFilter.location === "" || job.location.toLowerCase().includes(searchFilter.location.toLowerCase())
+
+        const newFilteredJobs = jobs.slice().reverse().filter(
+            job => matchesCategory(job) && matchesLocations(job) && matchesTitle(job) && matchesSearchLocation(job)
+        )
+
+        setFilteredJobs(newFilteredJobs)
+        setCurrentPage(1)
+    }, [jobs, selectedCategories, selectedLocations, searchFilter])
+
 
   return (
     <div className='container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8'>
@@ -52,7 +84,10 @@ const JobListing = () => {
                     {
                         JobCategories.map((category, index) => (
                             <li className='flex gap-3 items-center' key={index}>
-                                <input className='scale-125' type="checkbox" name='' id=''/>
+                                <input className='scale-125 accent-blue-600' type="checkbox"
+                                onChange={() => handleCategoryChange(category)}
+                                checked = {selectedCategories.includes(category)}
+                                />
                                 {category}
                             </li>
                         ))
@@ -67,7 +102,10 @@ const JobListing = () => {
                     {
                         JobLocations.map((location, index) => (
                             <li className='flex gap-3 items-center' key={index}>
-                                <input className='scale-125' type="checkbox" name='' id=''/>
+                                <input className='scale-125 accent-blue-600' type="checkbox"
+                                onChange={() => handleLocationChange(location)}
+                                checked = {selectedLocations.includes(location)}
+                                />
                                 {location}
                             </li>
                         ))
@@ -83,24 +121,24 @@ const JobListing = () => {
             <h3 className='font-medium text-3xl py-2' id='job-list'>Latest Jobs</h3>
             <p className='mb-8'>Get your desired job from top companies</p>
             <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4'>
-                {jobs.slice((currentPage-1)*6, currentPage*6).map((job, index) => (
+                {filteredJobs.slice((currentPage-1)*6, currentPage*6).map((job, index) => (
                     <JobCard key={index} job={job}/>
                 ))}
             </div>
 
             {/* Pagination */}
-            {jobs.length>0 && (
+            {filteredJobs.length>0 && (
                 <div className='flex items-center justify-center space-x-2 mt-10'>
                     <a href="#job-list">
                         <img onClick={() => setCurrentPage(Math.max(currentPage-1, 1))} src={assets.left_arrow_icon} alt="" />
                     </a>
-                    {Array.from({length:Math.ceil(jobs.length/6)}).map((_, index) => (
-                        <a href="#job-list">
+                    {Array.from({length:Math.ceil(filteredJobs.length/6)}).map((_, index) => (
+                        <a key={index} href="#job-list">
                             <button onClick={() => setCurrentPage(index+1)} className={`w-10 h-10 flex items-center justify-center border border-gray-300 rounded ${currentPage === index+1 ? 'bg-blue-100 text-blue-500' : 'text-gray-500'}`}>{index + 1}</button>
                         </a>
                     ))}
                     <a href="#job-list">
-                        <img onClick={() => setCurrentPage(Math.min(currentPage+1, Math.ceil(jobs.length/6)))} src={assets.right_arrow_icon} alt="" />
+                        <img onClick={() => setCurrentPage(Math.min(currentPage+1, Math.ceil(filteredJobs.length/6)))} src={assets.right_arrow_icon} alt="" />
                     </a>
                 </div>
             )}
--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
+import { assets } from "../assets/assets";
+import Loading from "../components/Loading";
+import Navbar from "../components/Navbar";
+import kconvert from 'k-converter'
+import moment from 'moment'
 
 const ApplyJob = () => {
-  return (
-    <div>ApplyJob</div>
-  )
-}
+  const { id } = useParams();
 
-export default ApplyJob
+  const [JobData, setJobData] = useState(null);
+
+  const { jobs } = useContext(AppContext);
+
+  const fetchJob = async () => {
+    const data = jobs.filter((job) => job._id === id);
+    if (data.length !== 0) {
+      setJobData(data[0]);
+      console.log(data[0]);
+    }
+  };
+
+  useEffect(() => {
+    if (jobs.length > 0) {
+      fetchJob();
+    }
+  }, [id, jobs]);
+
+  return JobData ? (
+    <>
+      <Navbar />
+      <div>
+        <div>
+          <div>
+            <div>
+              <img src={JobData.companyId.image} alt="" />
+              <div>
+                <h1>{JobData.title}</h1>
+                <div>
+                  <span>
+                    <img src={assets.suitcase_icon} alt="" />
+                    {JobData.companyId.name}
+                  </span>
+                  <span>
+                    <img src={assets.location_icon} alt="" />
+                    {JobData.location}
+                  </span>
+                  <span>
+                    <img src={assets.person_icon} alt="" />
+                    {JobData.level}
+                  </span>
+                  <span>
+                    <img src={assets.money_icon} alt="" />
+                    CTC : {kconvert.convertTo(JobData.salary)}
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <button>Apply Now</button>
+              <p>Posted {moment(JobData.date).fromNow()}</p>
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+    </>
+  ) : (
+    <Loading />
+  );
+};
+
+export default ApplyJob;
