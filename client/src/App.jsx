26d9d0bbbc003a0af5038ca2bea2754ebe18cdc2
Naman_ptdr <namanpatidar5555@gmail.com>
2025-06-07 17:25:10 +0530

    Pages Implementation - Dashboard, AddJob, ManageJobs, and ViewApplications   at Recruiter side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,12 @@ import ApplyJob from './pages/ApplyJob'
 import Applications from './pages/Applications'
 import RecruiterLogin from './components/RecruiterLogin'
 import { AppContext } from './context/AppContext'
+import Dashboard from './pages/Dashboard'
+import AddJob from './pages/AddJob'
+import ManageJobs from './pages/ManageJobs'
+import ViewApplications from './pages/ViewApplications'
+import 'quill/dist/quill.snow.css'
+
 const App = () => {
 
   const {showRecruiterLogin} = useContext(AppContext)
@@ -15,7 +21,12 @@ const App = () => {
         <Route path='/' element={<Home/>} />
         <Route path='/apply-job/:id' element={<ApplyJob/>} />
         <Route path='/applications' element={<Applications/>} />
+        <Route path='/dashboard' element={<Dashboard/>}>
+            <Route path='add-job' element={<AddJob/>}/>
+            <Route path='manage-jobs' element={<ManageJobs/>}/>
+            <Route path='view-applications' element={<ViewApplications/>}/>
 
+        </Route>
       </Routes>
     </div>
   )
--- client/src/pages/ViewApplication.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ViewApplication = () => {
-  return (
-    <div>ViewApplication</div>
-  )
-}
-
-export default ViewApplication
