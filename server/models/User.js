04dd9d728bc6e9747f9bcb867a9a7af8ab86763b
Naman_ptdr <namanpatidar5555@gmail.com>
2025-06-11 14:24:50 +0530

    server.js  and ignore node_modules

---
+++ server/models/User.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    _id: {type: String, required: true},
+    name: {type: String, requred: true},
+    email: {type: String, required: true, unique: true},
+    resume: {type: String},
+    image: {type: String, required: true}
+})
+
+const User = mongoose.model('User', userSchema)
+
+export default User;
